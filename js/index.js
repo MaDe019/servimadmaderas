377c288d77f3e05b210ce2f09ee44a7eed124caa
Matias Delgado <matiasdelgado.web@outlook.com>
2023-03-30 22:37:25 -0300

    v1 pagina preliminar

--- js/index.js
+++ js/index.js
@@ -1,10 +1,31 @@
 let prevScrollpos = window.pageYOffset;
+var header = document.getElementById("header");
+let showHeaderTimeout;
+
 window.addEventListener("scroll", function () {
   let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").classList.add("oculto");
+  if (prevScrollpos < currentScrollPos) {
+    header.classList.remove("oculto");
+    prevScrollpos = currentScrollPos;
   } else {
-    document.getElementById("header").classList.remove("oculto");
+    header.classList.add("oculto");
+    clearTimeout(showHeaderTimeout);
+    showHeaderTimeout = setTimeout(function () {
+      header.classList.remove("oculto");
+    }, 2000);
   }
-  prevScrollpos = currentScrollPos;
+});
+
+// función para que cuando se haga click en el icono del menu hamburguesa se extienda por 3 segundos mas mostrar el header
+var btnHeader = document.getElementById("btn-header");
+
+btnHeader.addEventListener("click", function () {
+  header.classList.add("oculto");
+});
+
+// función que se oculta header cuando haces click en uno de sus botones
+let navLink = document.getElementById("nav__link");
+
+navLink.addEventListener("click", function () {
+  header.classList.remove("oculto");
 });
